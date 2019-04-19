a6a93a19efade7e87d9a9d5890a5c7be5dd2e73e
Sean Carter <scarter0313@gmail.com>
2019-04-19 04:00:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ninjas_list = ["Naruto", "Sasuske","Obito", "Zabuto"]
+  ninjas_list << "Sub-Zero"
 end
 
 def add_element_to_start_of_array(array, element)
