00db3b249719c868d74cc81b6a5731e5c75d2b66
Sean Carter <scarter0313@gmail.com>
2019-04-19 04:06:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ninjas_list = ["wow", "Sasuske","Obito", "Zabuto"]
+  ninjas_list.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
