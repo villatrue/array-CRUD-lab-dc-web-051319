cb0bf598f719c53dfe96961f0ee555c81c4285f5
Sean Carter <scarter0313@gmail.com>
2019-04-19 04:05:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ninjas_list = ["Naruto", "Sasuske","Obito", "arrays!"]
   ninjas_list.pop
 end
 
