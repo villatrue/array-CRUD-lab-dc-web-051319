47f534b8008f3b5b8721e71b8fd63d6faeee87fc
Sean Carter <scarter0313@gmail.com>
2019-04-19 04:13:20 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ninjas_list = ["Naruto", "Sasuske","Obito", "Zabuto"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ninjas_list = ["Naruto", "Sasuske","Obito", "Zabuto"]
+  ninjas_list << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
+  ninjas_list = ["Naruto", "Sasuske","Obito", "Zabuto"]
+  ninjas_list.unshift("wow")
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ninjas_list = ["Naruto", "Sasuske","Obito", "arrays!"]
+  ninjas_list.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ninjas_list = ["wow", "Sasuske","Obito", "Zabuto"]
+  ninjas_list.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  array = ["wow", "I", "am", "really", "learning"]
+  array[2]
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
 end
